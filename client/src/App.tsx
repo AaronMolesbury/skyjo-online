215a86d3a3a96434af4482accac70186344086dc
aaronmolesbury <aaronmolesbury@gmail.com>
2024-05-25 21:46:48 +0100

    begin creating UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,43 @@
+import { createContext, useContext, useEffect, useState } from 'react'
 import './App.css'
+import { ISocketData } from './interfaces/ISocketData';
+import GameWindow from './ui/GameWindow';
+
+const WS_URL = "ws://localhost:8080";
+
+const WebSocketContext = createContext<WebSocket|null>(null);
+const DataContext = createContext<ISocketData|null>(null);
+
+export function useWebSocket() {
+  return useContext(WebSocketContext);
+}
+
+export function useData() {
+  return useContext(DataContext);
+}
 
 function App() {
+  const [data, setData] = useState<ISocketData|null>(null);
+  const [socket, setSocket] = useState<WebSocket|null>(null)
+  alert("deez");
+
+  useEffect(() => {
+    // Create WebSocket connection.
+    const ws = new WebSocket(WS_URL);
+    setSocket(ws);
+
+    // Listen for messages - update data in GameState context
+    ws.addEventListener("message", (event) => {
+      setData(JSON.parse(event.data as string));
+    });
+  })
+
   return (
-    <>
-    </>
+    <WebSocketContext.Provider value={socket}>
+      <DataContext.Provider value={data}>
+        <GameWindow/>
+      </DataContext.Provider>
+    </WebSocketContext.Provider>
   )
 }
 
---
+++ client/src/ui/GameWindow.tsx
@@ -0,0 +1,16 @@
+import "../css/GameWindow.css"
+
+function GameWindow() {
+    return (
+        <div className="GameWindow">
+            <div className="GameWindow_DeckWrapper">
+
+            </div>
+            <div className="GameWindow_ButtonWrapper">
+
+            </div>
+        </div>
+    )
+}
+
+export default GameWindow
